d23eb61ed1b6f591aa628ca498a52fce5b153434
Kaelo <Lentsoekaelo12090@gmail.com>
2024-02-29 15:02:15 +0200

    add

---
+++ numeric.py
@@ -0,0 +1,30 @@
+# Integer
+age = 24
+quantity = -7
+population = 100000
+
+# Floating-Point Type
+pi = 3.14159
+temperature = -10.5
+scientific_notation = 6.02e23 
+
+# Complex
+z1 =  2 + 3j
+z2 = -1.5 - 2.5j
+complex_result = z1 * z2
+
+# Print values
+print("Integer Type:")
+print("Age:", age)
+print("Quantity:", quantity)
+print("Population:", population)
+
+print("\nFloating-Point Type:")
+print("Pi:", pi)
+print("Temperature:", temperature)
+print("Scientific Notation", scientific_notation)
+
+print("\nComplex Type:")
+print("Complex Number 1:", z1)
+print("Complex Number 2:", z2)
+print("Complex Result:", complex_result)
